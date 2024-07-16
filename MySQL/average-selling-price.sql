b84064d09b71ba7fde6fa4769f763a7620892318
fawaz1298 <141839290+fawaz1298@users.noreply.github.com>
2024-07-16 17:08:41 +0530

    Update average-selling-price.sql

--- MySQL/average-selling-price.sql
+++ MySQL/average-selling-price.sql
@@ -3,8 +3,8 @@
 
 SELECT b.product_id, 
        ROUND(SUM(a.units * b.price) / SUM(a.units), 2) AS average_price 
-FROM   UnitsSold AS a 
-       INNER JOIN Prices AS b 
+FROM   Prices AS b 
+       INNER JOIN UnitsSold AS a
                ON a.product_id = b.product_id 
 WHERE  a.purchase_date BETWEEN b.start_date AND b.end_date 
 GROUP  BY product_id; 
